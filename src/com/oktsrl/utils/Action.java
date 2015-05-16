da222a97a962e43cf40a1645c489f3df7e3ffee3
Barbieri <barbieri@yahoo-inc.com>
2015-05-16 09:33:59 +0100

    GenerateTrainTest

--- src/com/oktsrl/utils/Action.java
+++ src/com/oktsrl/utils/Action.java
@@ -1,10 +1,22 @@
 package com.oktsrl.utils;
 
-public final class Action {
+import java.io.Serializable;
+
+public final class Action implements Comparable<Action>,Serializable{
     public int user;
     public int item;
     public double rating;
+    public long timestamp;
     
+    
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
     public int getUser() {
         return user;
     }
@@ -29,5 +41,50 @@ public final class Action {
         this.user = user;
         this.item = item;
         this.rating = rating;
+        this.timestamp=0L;
     }
+    
+    public Action(int user, int item, double rating,long timestamp) {
+        this.user = user;
+        this.item = item;
+        this.rating = rating;
+        this.timestamp=timestamp;
+    }
+
+    
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + item;
+        result = prime * result + user;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Action other = (Action) obj;
+        if (item != other.item)
+            return false;
+        if (user != other.user)
+            return false;
+        return true;
+    }
+
+    @Override
+    public int compareTo(Action o) {
+        if(timestamp<o.timestamp)
+            return -1;
+        if(timestamp>o.timestamp)
+            return 1;
+        return 0;
+    }
+    
 }
