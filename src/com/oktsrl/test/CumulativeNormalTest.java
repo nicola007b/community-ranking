29f762ed6c5790b0d58f963a9fd9c893e538e6d2
gmanco <manco.giuseppe@gmail.com>
2015-05-21 12:19:30 +0200

    Minor tests

--- src/com/oktsrl/test/CumulativeNormalTest.java
+++ src/com/oktsrl/test/CumulativeNormalTest.java
@@ -77,41 +77,4 @@ public class CumulativeNormalTest {
 	 
 	    return 0.5*(1.0 + sign*y);
 	}
-	 /*
-	void testPhi()
-	{
-	    // Select a few input values
-	    double x[] = 
-	    {
-	        -3, 
-	        -1, 
-	        0.0, 
-	        0.5, 
-	        2.1 
-	    };
-	 
-	    // Output computed by Mathematica
-	    // y = Phi[x]
-	    double y[] = 
-	    { 
-	        0.00134989803163, 
-	        0.158655253931, 
-	        0.5, 
-	        0.691462461274, 
-	        0.982135579437 
-	    };
-	 
-	        int numTests = sizeof(x)/sizeof(double);
-	 
-	    double maxError = 0.0;
-	    for (int i = 0; i < numTests; ++i)
-	    {
-	        double error = fabs(y[i] - phi(x[i]));
-	        if (error > maxError)
-	            maxError = error;
-	    }
-	 
-	        std::cout << "Maximum error: " << maxError << "\n";
-	} 
-	*/
 }
